171b4bd11eb1b3662185812f0d8a9c0eda05e596
Antony1060 <antoniostignjedec@gmail.com>
2022-01-23 11:06:26 +0100

    Better syntax highlighting

--- src/lib/layouts/Blog.svelte
+++ src/lib/layouts/Blog.svelte
@@ -1,8 +1,24 @@
 <script lang=ts>
-    import "../../../static/prism-material-gh.css"
+import { onMount } from "svelte";
+
+    import "../../../static/prism-material-mine.css"
     import type { Post } from "../../routes/posts/Post.type";
 
     export let post: Post;
+
+    let mdContainer: HTMLDivElement;
+
+    onMount(() => {
+        const codeBlocks = Array.from(mdContainer.querySelectorAll('pre[class*="language-"]'))
+                                .filter(it => it.className.startsWith("language-"));
+
+        for(const block of codeBlocks) {
+            const span = document.createElement("span");
+            span.innerText = block.className.slice("language-".length);
+            span.classList.add("code-block-name");
+            block.prepend(span);
+        }
+    })
 </script>
 
 <svelte:head>
@@ -22,7 +38,18 @@
     <h4>{post.created}</h4>
     <h4>{post.modified}</h4>
     <hr>
-    <slot />
+    <div bind:this={mdContainer}>
+        <slot />
+    </div>
     <hr>
     <h1>footer</h1>
 </div>
+
+<style>
+    :global(.code-block-name) {
+        display: block;
+        font-family: 'JetBrains Mono', monospace;
+        background-color: #1c2129;
+        width: 100%;
+    }
+</style>
