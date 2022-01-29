e4c34d7171155697a2e6779a682b72368e71fb25
Antony1060 <antoniostignjedec@gmail.com>
2022-01-29 16:07:29 +0100

    Update rss url origin

--- src/routes/rss.xml.ts
+++ src/routes/rss.xml.ts
@@ -25,12 +25,12 @@ export const get: RequestHandler = async ({ url }) => {
             <channel>
                 <title>antony.cloud</title>
                 <description>A blog! 🥵 // yes I know, I'm very creative with descriptions...</description>
-                <link>${url.origin}${url.pathname}</link>
+                <link>https://antony.cloud${url.pathname}</link>
                 <language>en-us</language>
                 ${posts.filter(it => !it.metadata.hidden).map(({ route, metadata: post }) => `
                 <item>
                     <title>${post.title}</title>
-                    <link>${url.origin}/posts/en/${route}</link>
+                    <link>https://antony.cloud/posts/en/${route}</link>
                     ${post.description ? `<description>${post.description}</description>` : ""}
                     <pubDate>${((date: Date) => 
                         `${dayNames[date.getDay()]}, ${padNumber(date.getDate())} ${monthNames[date.getMonth()]} ${date.getFullYear()} ${padNumber(date.getHours())}:${padNumber(date.getMinutes())}:${padNumber(date.getSeconds())} GMT`
