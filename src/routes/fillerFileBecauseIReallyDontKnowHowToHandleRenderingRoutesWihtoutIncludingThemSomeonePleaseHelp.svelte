dd9b032ea1d57a31d05ebb2862857b5f7311b7de
Antony1060 <antoniostignjedec@gmail.com>
2022-02-03 21:50:29 +0100

    Weird thing fix for something, someone please help

---
+++ src/routes/fillerFileBecauseIReallyDontKnowHowToHandleRenderingRoutesWihtoutIncludingThemSomeonePleaseHelp.svelte
@@ -0,0 +1,14 @@
+<script context="module" lang="ts">
+    import type { Load } from "@sveltejs/kit";
+
+    export const load: Load = async ({ fetch }) => {
+        await fetch("/rss.xml"); // literally just a fucking filler, because svelte doesn't want to parse rss.xml without it being called in a component, fuck this, wasted 40 minutes on this
+        await fetch("/allRaw.json")
+
+        return {}
+    }
+</script>
+
+<div>
+    Hi
+</div>
