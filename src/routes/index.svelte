99f34271452d5c5ce5561bf2d56c97ba0988cdf7
Antony1060 <antoniostignjedec@gmail.com>
2022-02-03 21:23:58 +0100

    Oops

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,7 +26,6 @@
     export const load: Load = async ({ fetch }) => {
         const posts: PostWithMeta[] = await fetch("/posts.json").then(res => res.clone().json());
         await fetch("/rss.xml"); // literally just a fucking filler, because svelte doesn't want to parse rss.xml without it being called in a component, fuck this, wasted 40 minutes on this
-        await fetch("/allRaw.json");
 
         posts
             .sort((a, b) => a.metadata.title.localeCompare(b.metadata.title))
